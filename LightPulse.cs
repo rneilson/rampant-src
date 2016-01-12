f501a94872446b997b3c3eca6e81fd510e14dde4
rneilson <rneilson@interloper.ca>
2016-01-12 16:47:26 -0500

    Started light pulse stuff

---
+++ LightPulse.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightPulse : MonoBehaviour {
+
+	public float intensityTarget = 2;
+	public float timeInitialTarget = 0.5;
+	public float timeTargetInitial = 0.5;
+	public bool holdAtTarget = false;
+	public bool looping = false;
+
+	private Light lightControl;
+	private float intensityStart;
+	private float intensityDiff;
+	private float counter;
+	private float phase;
+	private PulseMode mode;
+
+	private static float halfPi = Mathf.PI / 2;
+
+	// Use this for initialization
+	void Start () {
+		lightControl = GetComponent<Light>();
+		intensityStart = lightControl.intensity;
+		intensityDiff = intensityTarget - intensityStart;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+public enum PulseMode : byte {
+	ToTarget = 0,
+	FromTarget,
+	Holding,
+	Stopped
+}
