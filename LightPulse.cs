9c813b0215b2c3a1b96005a313616a3d6060b15e
rneilson <rneilson@interloper.ca>
2016-01-13 12:01:26 -0500

    Changed halfPi to const
    (Shoulda done that earlier)

--- LightPulse.cs
+++ LightPulse.cs
@@ -18,7 +18,7 @@ public class LightPulse : MonoBehaviour {
 	private float phase;
 	private PulseState currentState;
 
-	private static float halfPi = Mathf.PI / 2.0f;
+	private const float halfPi = Mathf.PI / 2.0f;
 
 	// Use this for initialization
 	void Start () {
