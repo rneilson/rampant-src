6f530df6db4b8803358c67bfceebca41282eee3e
rneilson <rneilson@interloper.ca>
2016-01-21 09:53:16 -0500

    Scrolling code a bit less temporary

--- ScrollCodeBox.cs
+++ ScrollCodeBox.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Globalization;
 using System.Text;
 using System;
 
@@ -10,26 +11,36 @@ public class ScrollCodeBox : MonoBehaviour {
 	private PulseControl timer;
 
 	// Textbox sizes
-	private int cols = 24;
-	private int rows = 24;	// Not including newline
+	private int cols = 24;	// Not including newline
 	private int rowlen;		// Including newline
+	private int rows = 24;
 	private int boxChars;
+	private int removeChars;
 
 	// Cursor state
-	private int cursorPos = 0;
-	private int cursorLine = 0;
-	private int cursorOffset = 0;
+	public int cursorPos = 0;	// Public for debug
+	public int cursorOld = 0;	// Public for debug
+	//private int cursorPos = 0;
+	//private int cursorLine = 0;
+	//private int cursorPage = 0;
+	//private int cursorOffset = 0;
 
 	// String data
 	private StringBuilder workhorse;
-	public String initialText;
-	public String availableChars;
+	public String initialText;		// Public for debug
+	public String availableChars;	// Public for debug
+
+	// Controls
+	public bool debugInfo = false;
+	public int removeLines = 24; 
 
 	// Use this for initialization
 	void Start () {
 		// Stupid compiler won't let me put these in initialization...
-		rowlen = rows + 1;
-		boxChars = cols * rowlen;
+		rowlen = cols + 1;
+		boxChars = rows * rowlen;
+		removeLines = (removeLines > rows) ? rows : removeLines;
+		removeChars = removeLines * rowlen;
 
 		// Grab components, obvs
 		display = GetComponent<TextMesh>();
@@ -37,10 +48,15 @@ public class ScrollCodeBox : MonoBehaviour {
 
 		// Stash initial text just in case
 		initialText = display.text;
+		display.text = "";
+
+		if (debugInfo) {
+			Debug.Log("initialText " + initialText.Length.ToString() 
+				+ " chars, boxChars " + boxChars.ToString(), gameObject);
+		}
 
 		// TEMP CODE
 		// Only here while I figure out what I've got access to
-		display.text = "";
 		CharacterInfo[] charArray = display.font.characterInfo;
 		int charArrayLen = charArray.Length;
 		workhorse = new StringBuilder(charArrayLen * 2);
@@ -51,18 +67,62 @@ public class ScrollCodeBox : MonoBehaviour {
 
 		// MORE TEMP CODE
 		// Just to get scrolling working
-		workhorse = new StringBuilder(initialText);
-
+		workhorse.Remove(0, workhorse.Length);
+		workhorse.EnsureCapacity(boxChars);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		cursorLine = timer.Loops;
-		cursorPos = Mathf.FloorToInt(timer.Phase * (float) rowlen);
-
 		// TEMP CODE
-		cursorOffset = ((cursorLine * rowlen) + cursorPos) % boxChars;
-		display.text = workhorse.ToString(0, cursorOffset);
+		UpdateDisplayText(initialText);
+	}
+
+	string GetCursorStr (string sourceStr) {
+		int cursorPage, cursorLine, cursorCol, cursorOffset;
+		string strToRet;
+
+		// Stash last cursor position
+		cursorOld = cursorPos;
+
+		// Find new cursor position
+		cursorPage = timer.Loops / rows;
+		cursorLine = timer.Loops % rows;
+		cursorCol = Mathf.FloorToInt(timer.Phase * (float) rowlen);
+		cursorPos = ((cursorPage * boxChars) + (cursorLine * rowlen) + cursorCol) % sourceStr.Length;
+
+		// Check if we've wrapped around
+		if (cursorPos < cursorOld) {
+			// Get remainder of source
+			strToRet = sourceStr.Substring(cursorOld, sourceStr.Length - cursorOld);
+			// Wrap around from start
+			strToRet += sourceStr.Substring(0, cursorPos);
+		}
+		else {
+			// Get difference between old and new cursor positions (might be nothing)
+			strToRet = sourceStr.Substring(cursorOld, cursorPos - cursorOld);
+		}
+
+		return strToRet;
+
+		// For later, as a reminder that corrupted letters might be multiple chars long
+		//return sourceStr.Substring(cursorOffset, 1);
+	}
+
+	void UpdateDisplayText (string sourceStr) {
+		// Append next char from source
+		workhorse.Append(GetCursorStr(sourceStr));
+		// Check if textbox is full, whittle down if so
+		if (workhorse.Length > boxChars) {
+			if (debugInfo) {
+				Debug.Log("Textbox wrapped around, length " + workhorse.Length.ToString(), gameObject);
+			}
+			workhorse.Remove(0, removeChars);
+			if (debugInfo) {
+				Debug.Log("New textbox string: \'" + workhorse.ToString() + "\'", gameObject);
+			}
+		}
+		// Update display
+		display.text = workhorse.ToString();
 	}
 
 	public Color GetCurrentColor () {
