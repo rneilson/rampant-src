babb40dbf9e24bb74e1ffc06fe20ac4b8c820b7a
rneilson <rneilson@interloper.ca>
2016-01-24 21:09:52 -0500

    Prelim work on EnemyList complete (?)

--- RedCubeGroundControl.cs
+++ RedCubeGroundControl.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 public class RedCubeGroundControl : MonoBehaviour {
 
@@ -272,39 +273,284 @@ public enum PredictionMode : byte {
 
 // Might as well be its own class
 public class EnemyList {
-	// Carried across all instances
+	// Shared between all instances (easier, frankly)
 	private static int enemyTypeIndex;						// Starting type number
-	private static List<EnemyType> enemyTypes;				// Master list
-	private static Dictionary<int, string> enemyTypeNames;	// Numbers->names
-	private static Dictionary<string, int> enemyTypeNums;	// Names->numbers
+	private static Dictionary<int, EnemyType> enemyTypeByNum;	// Numbers->names
+	private static Dictionary<string, EnemyType> enemyTypeByName;	// Names->numbers
 
 	// Per instance (several reasons to use them, not all the same)
+	private Dictionary<GameObject, EnemyInst> enemiesTotal;						// All tracked instances
+	private Dictionary<int, Dictionary<GameObject, EnemyInst>> enemiesByType;	// A dict of dicts, one per type
 
 	// Static constructor
 	static EnemyList () {
+		// Set up containers
 		enemyTypeIndex = 0;
-		enemyTypes = new List<EnemyType>();
-		enemyTypeNames = new Dictionary<int, string>();
-		enemyTypeNums = new Dictionary<string, int>();
+		enemyTypeByNum = new Dictionary<int, EnemyType>();
+		enemyTypeByName = new Dictionary<string, EnemyType>();
+
+		// Set first entry to EnemyType.Nothing
+		// This a) initializes the lists properly, and b) ensures proper lookups of EnemyType.Nothing
+		enemyTypeByNum.Add(EnemyType.Nothing.typeNum, EnemyType.Nothing);
+		enemyTypeByName.Add(EnemyType.Nothing.typeName, EnemyType.Nothing);
 	}
+
+	// Instance constructors and helpers
+	public EnemyList () {
+		InitializeList();
+	}
+
+	public EnemyList (List<EnemyInst> enemies) {
+		InitializeList();
+
+		if (enemies != null) {
+			foreach (EnemyInst enemy in enemies) {
+				this.Add(enemy);
+			}
+		}
+	}
+
+	public EnemyList (EnemyInst[] enemies) {
+		InitializeList();
+		
+		if (enemies != null) {
+			for (int i = 0; i < enemies.Length; i++) {
+				this.Add(enemies[i]);
+			}
+		}
+	}
+
+	private void InitializeList () {
+		this.enemiesTotal = new Dictionary<GameObject, EnemyInst>();
+		this.enemiesByType = new Dictionary<int, Dictionary<GameObject, EnemyInst>>();
+	}
+
+	// Static functions
+
+	// Few checks -- use carefully
+	private static EnemyType AddType (int newNum, string newName) {
+		if (enemyTypeByNum.ContainsKey(newNum)) {
+			throw new ArgumentOutOfRangeException(String.Format("typeNum {0} already in use!", newNum), "newNum");
+		}
+		if (enemyTypeByName.ContainsKey(newName)) {
+			throw new ArgumentOutOfRangeException(String.Format("typeName {0} already in use!", newName), "newName");
+		}
+		// Use given type index as num
+		EnemyType newType = new EnemyType(newNum, newName);
+		// Add to both indexes
+		enemyTypeByNum.Add(newType.typeNum, newType);
+		enemyTypeByName.Add(newType.typeName, newType);
+		// Advance enemyTypeIndex to next unused value
+		// This will ensure the next run will be consistent
+		while (enemyTypeByNum.ContainsKey(enemyTypeIndex)) {
+			enemyTypeIndex++;
+		}
+		// Aaaaand done
+		return newType;
+	}
+
+	private static EnemyType AddType (string newName) {
+		return AddType(enemyTypeIndex, newName);
+	}
+
+	// TODO: change to try/catch
+	public static EnemyType GetType (int typeNum) {
+		if (TypeExists(typeNum)) {
+			return enemyTypeByNum[typeNum];
+		}
+		else {
+			return EnemyType.Nothing;
+		}
+	}
+
+	// TODO: change to try/catch
+	public static EnemyType GetType (string typeName) {
+		if (typeName == "" ) {
+			throw new ArgumentNullException("Can't get unnamed enemy type", typeName);
+		}
+		else if (TypeExists(typeName)) {
+			// Check if it's already in there, return that if it is
+			return enemyTypeByName[typeName];
+		}
+		else {
+			return EnemyType.Nothing;
+		}
+	}
+
+	public static bool TypeExists (int typeNum) {
+		return enemyTypeByNum.ContainsKey(typeNum);
+	}
+
+	public static bool TypeExists (string typeName) {
+		return enemyTypeByName.ContainsKey(typeName);
+	}
+
+	// Public ways to check/add types (static, since the list is)
+	// Checks for dupes, so pretty safe to blindly use
+	public static EnemyType AddOrGetType (string typeName) {
+		// Null strings ain't gonna work
+		if (typeName == "") {
+			throw new ArgumentNullException("Can't add unnamed enemy type", typeName);
+		}
+		else if (TypeExists(typeName)) {
+			// Check if it's already in there, return that if it is
+			return GetType(typeName);
+		}
+		else {
+			// Not in there, add it
+			return AddType(typeName);
+		}
+	}
+
+	// Instance functions
+	public bool Add (EnemyInst inst) {
+		bool addedTotal = false, addedType = false;
+		// First, check type
+		if (!TypeExists(inst.typeNum)) {
+			// Enemy type not found, decide how to handle
+			if (inst.typeNum > 0) {
+				// Somehow unknown, let's add as such
+				AddType(inst.typeNum, "unknown" + inst.typeNum.ToString());
+			}
+			else {
+				// Non-type (0), not adding
+				return false;
+			}
+		}
+		// Type number exists (or it does now), now check if we have it on board
+		if (!this.HasType(inst.typeNum)) {
+			// Not already on board, add type num
+			enemiesByType.Add(inst.typeNum, new Dictionary<GameObject, EnemyInst>());
+		}
+		// Check if we already have inst...
+		// ...in total?
+		if (!enemiesTotal.ContainsKey(inst.gameObj)) {
+			// Add inst to overall dict
+			enemiesTotal.Add(inst.gameObj, inst);
+			addedTotal = true;
+		}
+		// ...by type?
+		if (!enemiesByType[inst.typeNum].ContainsKey(inst.gameObj)) {
+			// Add inst to per-type dict
+			enemiesByType[inst.typeNum].Add(inst.gameObj, inst);
+			addedType = true;
+		}
+
+		// Return true if added to either
+		return addedTotal || addedType;
+	}
+
+	public bool HasType (EnemyType enemyType) {
+		// Slightly redundant, but I'd like to be sure (it *is* a value type, after all)
+		return this.HasType(enemyType.typeNum) && this.HasType(enemyType.typeName);
+	}
+
+	public bool HasType (int typeNum) {
+		if (typeNum > 0) {
+			return enemiesByType.ContainsKey(typeNum);
+		}
+		else {
+			return false;
+		}
+	}
+
+	public bool HasType (string typeName) {
+		EnemyType retVal = GetType(typeName);
+		if (retVal) {
+			return HasType(retVal.typeNum);
+		}
+		else {
+			// Note that we will never have EnemyType.Nothing stored
+			return false;
+		}
+	}
+
 }
 
 public struct EnemyType {
-	public int typeNum;
-	public string typeName;
+	public readonly int typeNum;
+	public readonly string typeName;
+
+	// Empty type, returns false
+	public static readonly EnemyType Nothing = new EnemyType(0, "Nothing");
 
 	public EnemyType (int typeNum, string typeName) {
 		this.typeNum = typeNum;
 		this.typeName = typeName;
 	}
+
+	public override bool Equals (object obj) {
+		if (obj is EnemyType) {
+			return this.Equals((EnemyType) obj);
+		}
+		return false;
+	}
+
+	public static bool operator true (EnemyType x) {
+		return x.typeNum > 0 && x.typeName != "";
+	}
+
+	public static bool operator false (EnemyType x) {
+		return x.typeNum <= 0 || x.typeName == "";
+	}
+
+	public bool Equals (EnemyType x) {
+		return (typeNum == x.typeNum) && (typeName == x.typeName);
+	}
+
+	public override int GetHashCode () {
+		return typeNum.GetHashCode() ^ typeName.GetHashCode();
+	}
+
+	public static bool operator == (EnemyType lhs, EnemyType rhs) {
+		return lhs.Equals(rhs);
+	}
+
+	public static bool operator != (EnemyType lhs, EnemyType rhs) {
+		return !(lhs.Equals(rhs));
+	}
 }
 
 public struct EnemyInst {
-	public int typeNum;
-	public GameObject gameObj;
+	public readonly int typeNum;
+	public readonly GameObject gameObj;
+
+	// Empty type, returns false
+	public static readonly EnemyInst Nothing = new EnemyInst(0, null);
 
 	public EnemyInst (int typeNum, GameObject gameObj) {
 		this.typeNum = typeNum;
 		this.gameObj = gameObj;
+	}
+
+	public override bool Equals (object obj) {
+		if (obj is EnemyInst) {
+			return this.Equals((EnemyInst) obj);
+		}
+		return false;
+	}
+
+	public static bool operator true (EnemyInst x) {
+		return x.typeNum > 0 && x.gameObj != null;
+	}
+
+	public static bool operator false (EnemyInst x) {
+		return x.typeNum <= 0 || x.gameObj == null;
+	}
+
+	public bool Equals (EnemyInst x) {
+		return (typeNum == x.typeNum) && (gameObj == x.gameObj);
+	}
+
+	public override int GetHashCode () {
+		return typeNum.GetHashCode() ^ gameObj.GetHashCode();
+	}
+
+	public static bool operator == (EnemyInst lhs, EnemyInst rhs) {
+		return lhs.Equals(rhs);
+	}
+
+	public static bool operator != (EnemyInst lhs, EnemyInst rhs) {
+		return !(lhs.Equals(rhs));
 	}
 }
